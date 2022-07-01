0f44ecd4c6da277670e190a17d5c53b6c29577b8
Seth Matheson <smatheson@tenable.com>
2022-07-01 14:21:31 -0400

    initial push for t.sc support

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # tenb_scan_calendar
+
+Creates an .ics file that can be imported into your calendar management tool of choice to show when scans run and how long they're expected to take.
+
+t.sc supported initially, t.io support in future release
+
+### Config
+Sample .env file in same folder as script:
+
+```
+SC_ACCESS_KEY=aaaaaaabbbbbbccccddddeeeffff1234  
+SC_SECRET_KEY=aaaaaaabbbbbbccccddddeeeffff1234   
+SC_ADDRESS=10.0.0.102  
+SC_PORT=443  
+```
+### Requirements
+* Must be T.sc 5.13 or above.
+* Must enable API auth application wide
+* Must create or use existing API keys for a user that can "Create Scans" as part of their role (Vulnerability Analyst, Security Analyst, Security Manager or custom) and that can manage all objects for all groups.
+* Initially only works with network scans (not agents)
+* Must be run once per tenable.sc 'organization' (and rename the ics file after each run)
+
+Tested on python 3.10; will likely work on 3.6+
--- scan_calendar.py
+++ scan_calendar.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python
 
-from ics import Event, Calendar
-from ics.parse import ContentLine
+from ics import Event, Calendar, ContentLine
 import logging
-from tenable.io import TenableIO
+#from tenable.io import TenableIO
+from tenable.sc import TenableSC
 from decouple import config, UndefinedValueError
 from datetime import datetime
+from dateutil import parser
+import ast
 import pytz
+import os
 from pprint import pprint
 
 #logging.basicConfig(level=logging.CRITICAL)
 
 # Set some variables that need setting (pulled from .env file passed to container or seen locally in the same folder as script)
 try:
-    tio_address = config('TIO_ADDRESS', default="https://cloud.tenable.com")
-    tio_access_key = config('TIO_ACCESS_KEY')
-    tio_secret_key = config('TIO_SECRET_KEY')
+    sc_address = config('SC_ADDRESS')
+    sc_access_key = config('SC_ACCESS_KEY')
+    sc_secret_key = config('SC_SECRET_KEY')
+    sc_port = config('SC_PORT', default=443)
     debug_set = config('DEBUG', cast=bool, default=False)
 except UndefinedValueError as err:
     print("Please review the documentation and define the required connection details in an environment file.")
     print()
     raise SystemExit(err)
+
+# Login to Tenable.sc
+def tsc_login():
+    try:
+        sc = TenableSC(sc_address, port=sc_port, access_key=sc_access_key, secret_key=sc_secret_key, backoff=5.0, timeout=10)
+    except (NameError, TimeoutError) as err:
+        print("Please verify connection details.")
+        exit()
+    except (ConnectionError) as err:
+        raise SystemExit(err)
+    return sc
+
+
+
+
 
 # Debugging bit
 if debug_set is True:
@@ -31,8 +50,10 @@ else:
 # Let's initialize the calendar before anything else
 c = Calendar()
 
-# Try to login to T.io
-tio = TenableIO(tio_access_key, tio_secret_key)
+#Login to stuff
+
+sc = tsc_login()
+
 
 # Define Functions
 def get_timezone(tz_str):
@@ -66,35 +87,39 @@ def convert_unix_time(time):
 def list_avg(lst):
     return sum(lst) / len(lst)
 
-def gen_event(name, rrules, starttime, timezone, creation_date, owner, scan_type):
+def gen_event(name, rrules, starttime, timezone, creation_date, owner, scan_type, description):
     # intialize a new event
     e = Event()
 
     # Set the event name
-    e.name = name
+    e.summary = name
 
     # Set the start time
-    e.begin = starttime_utc
+    e.begin = parser.parse(starttime_utc)
 
     if endtime_utc:
-        e.end = endtime_utc
+        e.end = parser.parse(endtime_utc)
 
     e.url = "https://cloud.tenable.com"
 
     if scan_type == "Agent":
         scan_length_desc = "The agent scan window has been set in the scan job."
     elif scan_type == "Network" and estimated_run is True:
-        scan_length_desc = "The scan length has been estimated based on the average of the previously run jobs."
+        scan_length_desc = "The scan time has been estimated based on the average of the previously run jobs."
+    elif scan_type == "Network" and estimated_run is False and endtime_utc:
+        scan_length_desc = "The max scan time has been set in the job."
     else:
-        scan_length_desc = "The duration of the calendar event for this job has defaulted to 1 hour, as we cannot yet estimate how long it will take."
+        scan_length_desc = "The duration of the calendar event for this job has defaulted to 1 hour, as we cannot yet estimate how long it will take and no max has been set."
 
     e.description = "Scan Owner: " + owner + "\n" + \
+            "Scan Description: " + description + "\n" + \
             "Scan Type: " + scan_type + "\n" + \
+            scan_targets + "\n" + \
             "Scan Length: " + scan_length_desc
 
     
     # Set the schedule, if it exists
-    if "FREQ=ONETIME" not in rrules:
+    if "FREQ=ONETIME" not in rrules and rrules != "":
         rrule_content = ContentLine(name='RRULE', params={}, value=rrules)
         e.extra.append(rrule_content)
  
@@ -103,22 +128,29 @@ def gen_event(name, rrules, starttime, timezone, creation_date, owner, scan_type
     e.extra.append(dtstamp_content)
 
     # add all to events
-    c.events.add(e)
+    c.events.append(e)
 
 # All the actual processing goes here
-for scan in tio.scans.list():
-    if '{enabled}'.format(**scan) == "True":
-        tz_format = get_timezone('{timezone}'.format(**scan))
-        dst = is_dst('{timezone}'.format(**scan), convert_dt_obj('{starttime}'.format(**scan)))
+for scan in sc.scans.list(['id','name','schedule', 'createdTime', 'owner','maxScanTime', 'ipList', 'assets', 'description'])['manageable']:
+    if '{schedule[enabled]}'.format(**scan) == "true":
+        raw_timezone = '{schedule[start]}'.format(**scan).split('=')[1].split(':')[0]
+        raw_start = '{schedule[start]}'.format(**scan).split('=')[1].split(':')[1]
+        tz_format = get_timezone(raw_timezone)
+        dst = is_dst(raw_timezone, convert_dt_obj(raw_start))
         
-        starttime_utc_dt = local_datetime(convert_dt_obj('{starttime}'.format(**scan)), tz_format, dst)
+        starttime_utc_dt = local_datetime(convert_dt_obj(raw_start), tz_format, dst)
         starttime_utc = dt_to_utc(starttime_utc_dt)
 
         # We're going to have to do fun things to determine scan endtimes.
-        if scan.get('type') == "agent":
+        if '{maxScanTime}'.format(**scan) != 'unlimited':
+            endtime = (int('{maxScanTime}'.format(**scan)) * 60 * 60)
+            endtime_utc = convert_unix_time(int(starttime_utc_dt.timestamp()) + endtime)
+            estimated_run = False
+            scan_type = "Network"
+        elif scan.get('type') == "agent":
             scan_type = "Agent"
             scan_editor_id_path = "editor/scan/" + '{id}'.format(**scan)
-            scan_details_resp = tio.get(scan_editor_id_path)
+            scan_details_resp = sc.get(scan_editor_id_path)
             scan_details = scan_details_resp.json().get('settings').get('basic').get('inputs')
             for item in scan_details:
                 if item.get('id') == "scan_time_window":
@@ -130,10 +162,12 @@ for scan in tio.scans.list():
             endtime_utc = None
             estimated_run = False
             scan_type = "Network"
+            '''
             # if the scan isn't a one-off, check to see if there is a history 
             # we can use to guess the time it's going to take to run.
-            if scan.get('rrules') != "FREQ=ONETIME":
-                scan_history = tio.scans.history(scan.get('id'))
+            
+            if scan.get('schedule').get('repeatRule') != "FREQ=ONETIME":
+                scan_history = sc.scans.history(scan.get('id'))
                 scan_duration = []
                 scan_duration_count = 5
                 scan_duration_iter = 0
@@ -146,16 +180,25 @@ for scan in tio.scans.list():
                 endtime = list_avg(scan_duration)
                 endtime_utc = convert_unix_time(int(starttime_utc_dt.timestamp()) + endtime)
                 estimated_run = True
-     
-        #print(scan)
+            '''
+
+
+        asset_list = ""
+        asset_targets = ast.literal_eval('{assets}'.format(**scan))
+        if len(asset_targets) > 0:
+            for x in asset_targets:
+                asset_list+= x['name'] + " "
+        scan_targets = "Targeted Assets: " + asset_list + "\n" + \
+            "Targeted IPs: " + '{ipList}'.format(**scan)
         gen_event(
                 '{name}'.format(**scan), \
-                '{rrules}'.format(**scan), \
-                '{starttime}'.format(**scan), \
-                '{timezone}'.format(**scan), \
-                int('{creation_date}'.format(**scan)), \
-                '{owner}'.format(**scan), \
-                scan_type
+                '{schedule[repeatRule]}'.format(**scan), \
+                raw_start, \
+                raw_timezone, \
+                int('{createdTime}'.format(**scan)), \
+                '{owner[username]}'.format(**scan), \
+                scan_type, \
+                '{description}'.format(**scan), \
                 )
 
 #for policy in tio.policies.list():
@@ -165,5 +208,5 @@ for scan in tio.scans.list():
 c.events
 
 # Write out the calendar file
-with open('tenableio_scans.ics', 'w') as my_file:
+with open('tenablesc_scans.ics', 'w') as my_file:
     my_file.writelines(c)
